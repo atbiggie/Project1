245b594a4aee81385451d56cb35eab5ccc371d1e
Autumn Biggie <atbiggie@ncsu.edu>
2021-10-04 22:19:04 -0400

    attempt to display leaflet

---
+++ RenderFile.R
@@ -0,0 +1,4 @@
+#code to render Biggie_WeatherAPI_Project.Rmd
+
+rmarkdown::render(input = "Biggie_WeatherAPI_Project.Rmd",  output_format = "github_document", output_file = "README.md", output_dir = "C:/Users/autum/Documents/ST 558/Project1", output_options = list(toc = TRUE, toc_depth = 3))
+
